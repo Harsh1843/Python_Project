90c6d788c7f961742f91c618fac5c7a446e7f020
Harsh1843 <85788384+Harsh1843@users.noreply.github.com>
2023-10-20 20:17:48 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route("/")
+def index():
+  #This function is the handler for the root URL("/")
+  return "Hello World"
+if __name__=="__main__":
+  #Run the Flask application if the script is executed directly
+  run.app()
